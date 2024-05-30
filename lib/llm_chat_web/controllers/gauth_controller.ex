a0281f29686bbce56bf3abc02cb2c8d48e1af0d9
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-05-30 10:42:49 +0000

    validate user session

--- lib/llm_chat_web/controllers/gauth_controller.ex
+++ lib/llm_chat_web/controllers/gauth_controller.ex
@@ -1,14 +1,15 @@
 defmodule LlmChatWeb.GauthController do
   use LlmChatWeb, :controller
 
+  require Logger
   alias LlmChatWeb.UserGauth
 
-  @spec callback(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def callback(conn, %{"code" => code}) do
     {:ok, token} = ElixirAuthGoogle.get_token(code, conn)
     {:ok, profile} = ElixirAuthGoogle.get_user_profile(token.access_token)
     user = LlmChat.Contexts.User.upsert!(profile)
-    conn |> UserGauth.log_in_user(user) |> render(:welcome, user: user)
+    gauth_expiration = DateTime.now!("Etc/UTC") |> DateTime.add(token.expires_in, :second)
+    conn |> UserGauth.log_in_user(user, gauth_expiration) |> render(:welcome, user: user)
   end
 
   def logout(conn, _params) do
