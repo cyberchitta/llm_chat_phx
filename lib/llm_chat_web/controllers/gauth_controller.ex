719df7b1082ac5a26e07cdcadcd95210907aca9a
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-07-20 17:09:41 +0000

    use buitlin liveview session expiration

--- lib/llm_chat_web/controllers/gauth_controller.ex
+++ lib/llm_chat_web/controllers/gauth_controller.ex
@@ -8,8 +8,7 @@ defmodule LlmChatWeb.GauthController do
     {:ok, token} = ElixirAuthGoogle.get_token(code, conn)
     {:ok, profile} = ElixirAuthGoogle.get_user_profile(token.access_token)
     user = LlmChat.Contexts.User.upsert!(profile)
-    gauth_expiration = DateTime.now!("Etc/UTC") |> DateTime.add(token.expires_in, :second)
-    conn |> UserGauth.log_in_user(user, gauth_expiration) |> render(:welcome, user: user)
+    conn |> UserGauth.log_in_user(user) |> render(:welcome, user: user)
   end
 
   def logout(conn, _params) do
