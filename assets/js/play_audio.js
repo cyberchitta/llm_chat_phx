7859034ec063612e308264f9cd9f94cad1d50cdb
restlessronin <88921269+restlessronin@users.noreply.github.com>
2024-08-05 06:33:37 +0000

    replace svg with css

--- assets/js/play_audio.js
+++ assets/js/play_audio.js
@@ -158,22 +158,20 @@ export default {
     button.classList.remove("loading");
     button.disabled = false;
 
-    let icon;
+    let iconClass;
     switch (state) {
       case "play":
-        icon =
-          '<path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />';
+        iconClass = "hero-speaker-wave";
         break;
       case "stop":
-        icon =
-          '<path stroke-linecap="round" stroke-linejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />';
+        iconClass = "hero-stop";
         break;
       case "loading":
         button.classList.add("loading");
-        icon = "";
+        iconClass = "";
         break;
     }
 
-    button.innerHTML = `<span class="flex h-[30px] w-[30px] items-center justify-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">${icon}</svg></span>`;
+    button.innerHTML = `<span class="flex h-[30px] w-[30px] items-center justify-center"><span class="h-4 w-4 ${iconClass}"></span></span>`;
   },
 };
